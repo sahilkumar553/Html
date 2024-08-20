50c0108563068905ba22655491fda1361280d029
Sahil Kumar Gupta <sahilkumargupta553@gmail.com>
2024-08-21 01:48:00 +0530

    Initial commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class Hello{
+    public static void main(String[] args) {
+        System.out.println("Sahil kumar Gupta");
+        System.out.println(new Date());
+    }
+}
