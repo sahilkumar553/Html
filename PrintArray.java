fb407fe33c91c9fd17ddb546348da841cd4a26ae
Sahil Kumar Gupta <sahilkumargupta553@gmail.com>
2024-08-21 02:17:11 +0530

    Array

---
+++ PrintArray.java
@@ -0,0 +1,11 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        // Define an array of integers
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Loop through the array and print each element
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
